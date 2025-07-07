22a49a7dbd7e71fc669d3754957df257faa9b4e5
Milton Guillermo Rivas Palacios <00019725@uca.edu.sv>
2025-07-07 11:40:11 -0600

    cambios de .h

---
+++ Juego_completo/src/menu.h
@@ -0,0 +1,11 @@
+#ifndef MENU_H
+#define MENU_H
+
+void enableANSI();
+void clearScreen();
+void mostrarEncabezado();
+int menuInteractivo();
+void startGame();
+void mostrarEstadisticas();
+
+#endif
